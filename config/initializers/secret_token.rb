c4e42e520d9807b32678b620223c6ffb6a750ca2
yasir saleem <yasrkurdish@gmail.com>
2017-04-11 15:34:24 +0300

    adding for first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphaBlog::Application.config.secret_token = 'fbec39514bb63f74e4a4ef2adb840ca1696b33e8e0ebeeefd6815fb143d617d4673828c366de15bf506d30488b16cbf74a51e96337a7f29181ad80374097fc50'
